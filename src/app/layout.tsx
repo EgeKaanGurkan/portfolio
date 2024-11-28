d835f9a60064b587d0bb899250510c7384866332
EgeKaanGurkan <ege.gurkan2608@gmail.com>
2024-11-28 23:12:10 +0100

    feat: vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import React from "react";
+import {Analytics} from "@vercel/analytics/vue";
 
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
+      <Analytics />
       <ThemeProvider
         attribute="class"
         defaultTheme="dark"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,28 +19,28 @@ const aleo = Aleo({
   subsets: ["latin"]
 })
 
-const PulsingDot = () => (
-  <motion.div
-    className="flex items-center space-x-2 mb-5"
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5 }}
-  >
-    <motion.div
-      className="w-3 h-3 bg-green-500 rounded-full"
-      animate={{
-        scale: [1, 1.2, 1],
-        opacity: [0.7, 1, 0.7],
-      }}
-      transition={{
-        duration: 1.5,
-        repeat: Infinity,
-        ease: "easeInOut",
-      }}
-    />
-    <span className="text-sm ">Currently browsing jobs in London.</span>
-  </motion.div>
-)
+// const PulsingDot = () => (
+//   <motion.div
+//     className="flex items-center space-x-2 mb-5"
+//     initial={{ opacity: 0, y: 20 }}
+//     animate={{ opacity: 1, y: 0 }}
+//     transition={{ duration: 0.5 }}
+//   >
+//     <motion.div
+//       className="w-3 h-3 bg-green-500 rounded-full"
+//       animate={{
+//         scale: [1, 1.2, 1],
+//         opacity: [0.7, 1, 0.7],
+//       }}
+//       transition={{
+//         duration: 1.5,
+//         repeat: Infinity,
+//         ease: "easeInOut",
+//       }}
+//     />
+//     <span className="text-sm ">Currently browsing jobs in London.</span>
+//   </motion.div>
+// )
 
 export default function Home() {
   console.log(siStackoverflow)
